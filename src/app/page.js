66c922e43b31e955a3311d2666367941cca169d5
izaack107 <izaack107@gmail.com>
2023-11-09 09:42:50 -0600

    Solucion de error location

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,15 @@ import Image from 'next/image'
 import Master from './components/main'
 import Description from './components/description'
 import Certificates from './components/certificates'
-import {useClient} from 'next/client'
 
 export default function Home() {
-  const isClient = useClient();
   return (
     <>
       <Master />
       <div className="container mx-auto pb-12"/>
       <Description />
       <div className="container mx-auto pb-12"/>
-      {isClient && <Certificates />}
-
+      <Certificates />
     </>
   )
 }
