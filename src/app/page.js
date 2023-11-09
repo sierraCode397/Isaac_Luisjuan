e093bf8698909aa0039863ffdb8f65a7ac8ebdcd
izaack107 <izaack107@gmail.com>
2023-11-09 16:25:56 -0600

    Footer añadido

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Master from './components/main'
 import Description from './components/description'
 import Certificates from './components/certificates'
+import Footer from './components/footer'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Description />
       <div className="container mx-auto pb-12"/>
       <Certificates />
+      <Footer />
     </>
   )
 }
