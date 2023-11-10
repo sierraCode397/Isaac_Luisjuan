6b80707c41ef131554d40c1e593ab8e3ea6d9cb3
izaack107 <izaack107@gmail.com>
2023-11-09 19:57:20 -0600

    Añadiendo portafolio de proyectos

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Master from './components/main'
 import Description from './components/description'
 import Certificates from './components/certificates'
 import Footer from './components/footer'
+import Portafolio from "./components/portafolio";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <Master />
       <div className="container mx-auto pb-12"/>
       <Description />
+      <div className="container mx-auto pb-12"/>
+      <Portafolio />
       <div className="container mx-auto pb-12"/>
       <Certificates />
       <Footer />
