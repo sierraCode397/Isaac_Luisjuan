c627853f73e9c0eb56d02dd003163c20b58c3726
izaack107 <izaack107@gmail.com>
2023-11-16 15:03:30 -0600

    Update Portafolio and Footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => {
                             <img className="w-auto h-7" src="https://imgur.com/e2TOmcF.jpg" alt="" />
                         </a>
 
-                        <p className="max-w-sm mt-2 text-gray-500 dark:text-gray-400">Join 31,000+ other and never miss out on new tips, tutorials, and more.</p>
+                        <p className="max-w-sm mt-2 text-gray-500">Join 31,000+ other and never miss out on new tips, tutorials, and more.</p>
 
                         <div className="flex mt-6 -mx-2">
                             <a href="#"
-                                className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                                className="mx-2 text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:hover:text-blue-400"
                                 aria-label="WhatsApp">
                                 <img className="w-5 h-5 fill-current" src="https://imgur.com/VloprFd.jpg">
 
@@ -25,7 +25,7 @@ const Footer = () => {
 
                         
                             <a href="#"
-                                className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                                className="mx-2 text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:hover:text-blue-400"
                                 aria-label="Facebook">
                                 <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -35,7 +35,7 @@ const Footer = () => {
                             </a>
 
                             <a href="#"
-                                className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                                className="mx-2 text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:hover:text-blue-400"
                                 aria-label="WhatsApp">
                                 <img className="w-5 h-5 fill-current" src="https://imgur.com/JnZ4Jfb.jpg">
 
@@ -43,7 +43,7 @@ const Footer = () => {
                             </a>
                         
                             <a href="#"
-                                className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                                className="mx-2 text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:hover:text-blue-400"
                                 aria-label="Github">
                                 <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -58,30 +58,30 @@ const Footer = () => {
                 <div className="mt-6 lg:mt-0 lg:flex-1">
                     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                         <div>
-                            <h3 className="text-gray-700 uppercase dark:text-white">About</h3>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Company</a>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">community</a>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Careers</a>
+                            <h3 className="uppercase text-white">About</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Company</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">community</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Careers</a>
                         </div>
 
                         <div>
-                            <h3 className="text-gray-700 uppercase dark:text-white">Blog</h3>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Tec</a>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Music</a>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Videos</a>
+                            <h3 className="uppercase text-white">Blog</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Tec</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Music</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Videos</a>
                         </div>
 
                         <div>
-                            <h3 className="text-gray-700 uppercase dark:text-white">Products</h3>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Mega cloud</a>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Aperion UI</a>
-                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Meraki UI</a>
+                            <h3 className="uppercase text-white">Products</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Mega cloud</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Aperion UI</a>
+                            <a href="#" className="block mt-2 text-sm text-gray-300 hover:underline">Meraki UI</a>
                         </div>
 
                         <div>
-                            <h3 className="text-gray-700 uppercase dark:text-white">Contact</h3>
-                            <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">+1 526 654 8965</span>
-                            <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">example@email.com</span>
+                            <h3 className="uppercase text-white">Contact</h3>
+                            <span className="block mt-2 text-sm text-gray-300 hover:underline">+52 341 411 2817</span>
+                            <span className="block mt-2 text-sm text-gray-300 hover:underline">isaacluisjuan0@email.com</span>
                         </div>
                     </div>
                 </div>
@@ -90,7 +90,7 @@ const Footer = () => {
             <hr className="h-px my-6 bg-gray-200 border-none dark:bg-gray-700" />
 
             <div>
-                <p className="text-center text-gray-500 dark:text-gray-400">© Brand 2020 - All rights reserved</p>
+                <p className="text-center text-gray-500">© SierraCode397 2023 - All rights reserved</p>
             </div>
         </div>
     </footer>
--- src/app/components/portafolio.js
+++ src/app/components/portafolio.js
@@ -13,31 +13,41 @@ const Portafolio = () => {
   return (
     <section className="bg-gray-900">
         <div className="container px-6 py-10 mx-auto">
-            <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Portfolio
+            <h1 className="text-2xl font-semibold text-center capitalize lg:text-4xl text-white">Portfolio
             </h1>
 
-            <p className="mt-4 text-center text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
+            <p className="mt-4 text-center text-gray-200">
+                Welcome to the project gallery. Below I present a selected sample of my most recent works, each of which signifies a commitment to continuous learning and improving the quality of my software.
             </p>
 
-
             <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+                
                 {sortedItems?.map((proyect) => (
 
-                    <div key={proyect.name} className="overflow-hidden bg-cover rounded-lg h-96 group relative"
-                    style={{ backgroundImage: `url(${proyect.image})`, backgroundPosition: 'center' }}>
-
-                        <div className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                            <p className="mt-2 text-sm tracking-wider font-semibold text-blue-400 uppercase hover:text-blue-800 cursor-pointer">{proyect.name}</p>
-                            <h2 className="mt-4 text-lg font-normal font-sans text-white ">{proyect.description}</h2>
+                    <div
+                    key={proyect.name}
+                    className="overflow-hidden bg-cover rounded-lg h-96 group relative"
+                    style={{
+                        backgroundImage: `url(${proyect.image})`,
+                        backgroundSize: '100% 100%', // Ajusta el tamaño de la imagen para cubrir el contenedor
+                        backgroundPosition: 'center', // Centra la imagen en el contenedor
+                        backgroundRepeat: 'no-repeat', // Evita que la imagen se repita
+                    }}
+                    >  
+                        <div className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-900/70 group-hover:opacity-100">
+                            <span class="relative flex h-3 w-3">
+                                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                                <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                            </span>
+                            <p className="mt-4 text-lg tracking-wider font-extrabold text-blue-500 uppercase hover:animate-bounce ">{proyect.name}</p>
+                            <h2 className="mt-2 text-lg font-normal font-sans text-white ">{proyect.description}</h2>
 
                             {/* Aplicamos cursor-pointer solo a las etiquetas <a> */}
-                            <a href={proyect.code} className="mt-2 text-lg tracking-wider font-semibold text-blue-400 uppercase hover:text-blue-600 cursor-pointer">Code</a>
-                            <a href={proyect.link} className="mt-2 text-lg tracking-wider font-semibold text-blue-400 uppercase hover:text-blue-600 cursor-pointer">Website</a>
+                            <a href={proyect.code} className="mt-2 text-lg tracking-wider font-semibold text-blue-600 uppercase hover:text-blue-900 cursor-pointer">Code</a>
+                            <a href={proyect.link} className="mt-2 text-lg tracking-wider font-semibold text-blue-600 uppercase hover:text-blue-900 cursor-pointer">Website</a>
                         </div>
                     </div>
                   ))}
-
             </div>
         </div>
     </section>
