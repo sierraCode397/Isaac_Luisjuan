1dbacc00bb6c423778434c3432be526cec22d3ac
izaack107 <izaack107@gmail.com>
2023-11-17 14:41:52 -0600

    Update description

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'IsaacLuisjuan',
+  title: 'Isaac Luisjuan',
   description: 'Portafolio personal de precentacion',
 }
 
