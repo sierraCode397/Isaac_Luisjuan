cd0931dc3abe2d4f719e81bb0704c46201a06abb
izaack107 <izaack107@gmail.com>
2023-11-17 18:47:46 -0600

    Update main [Descriptions and padding]

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -1,7 +1,7 @@
 'use client'
 
-
 import React from "react";
+import Link from 'next/link';
 
 import Navbar from "./../common/nav";
 
@@ -43,8 +43,8 @@ export default function Profile() {
             </svg>
           </div>
         </section>
-        <section className="relative py-16 bg-gray-300">
-          <div className="container mx-auto px-4">
+        <section className="relative py-16 bg-gray-900">
+          <div className="container mx-auto px-16">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
@@ -60,13 +60,14 @@ export default function Profile() {
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
-                      <button
-                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+                      <Link
+                        href='https://mx.linkedin.com/in/isaac-luisjuan-guerrero-b016b0258'
+                        className="bg-blue-400 active:bg-gray-900 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                         type="button"
                         style={{ transition: "all .15s ease" }}
                       >
                         Connect
-                      </button>
+                      </Link>
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
@@ -75,19 +76,19 @@ export default function Profile() {
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
                           22
                         </span>
-                        <span className="text-sm text-gray-500">Friends</span>
+                        <span className="text-sm text-gray-500">Technologies</span>
                       </div>
                       <div className="mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
                           10
                         </span>
-                        <span className="text-sm text-gray-500">Photos</span>
+                        <span className="text-sm text-gray-500">Certifications</span>
                       </div>
                       <div className="lg:mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
                           89
                         </span>
-                        <span className="text-sm text-gray-500">Comments</span>
+                        <span className="text-sm text-gray-500">Projects</span>
                       </div>
                     </div>
                   </div>
@@ -96,11 +97,15 @@ export default function Profile() {
                   <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
                     Isaac Luisjuan G.
                   </h3>
-                  <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
+                  <div className="text-sm leading-normal mt-0 mb-8 text-gray-500 font-bold uppercase">
                     <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
                     Cd. Guzman, Jalisco, Mexico
                   </div>
-                  <div className="mb-2 text-gray-700 mt-10">
+
+                  <div className=" border-t border-gray-300 text-center">
+                  </div> 
+                  
+                  <div className="mb-2 text-gray-700 mt-8">
                     <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
                     Full Stack Debeloper
                   </div>
@@ -111,7 +116,7 @@ export default function Profile() {
                 </div>
                 {/*                 
                   <div className="mt-10 py-10 border-t border-gray-300 text-center">
-                    {/* ----------------------------------- contenido 
+                     ----------------------------------- contenido 
                   </div> 
                 */}
               </div>
