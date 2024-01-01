bcbf52a03bc7436c7106d0e2d2ed7985c30a478e
izaack107 <izaack107@gmail.com>
2024-01-01 01:16:35 -0600

    ADD TAGCeparate

---
+++ src/app/helper/TagCeparate.js
@@ -0,0 +1,14 @@
+export default function TagCeparate() {
+  return (
+    <>
+        <div className=" mx-2 pt-1 bg-white w-52 rounded-xl" />
+        <div className=" mx-2 bg-green-900 w-32 rounded-xl" />
+        <div className=" mx-2 bg-white w-44 rounded-xl" />
+        <div className=" mx-2 bg-green-900 w-52 rounded-xl" />
+        <div className=" mx-2 bg-white w-32 rounded-xl" />
+        <div className=" mx-2 bg-green-900 w-20 rounded-xl" />
+        <div className=" mx-2 bg-white w-44 rounded-xl" />
+        <div className=" mx-2 bg-green-900 w-16 rounded-xl" />
+    </>
+  );
+}
