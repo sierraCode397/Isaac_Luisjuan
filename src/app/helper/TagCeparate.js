7f7788f1818f54d664940954fb6164fc6ce6f734
izaack107 <izaack107@gmail.com>
2024-01-03 15:40:57 -0600

    Update TagCeparate to desktop

--- src/app/helper/TagCeparate.js
+++ src/app/helper/TagCeparate.js
@@ -2,13 +2,17 @@ export default function TagCeparate() {
   return (
     <>
         <div className=" mx-2 pt-1 bg-white w-52 rounded-xl" />
-        <div className=" mx-2 bg-green-600 w-32 rounded-xl" />
+        <div className=" mx-2 bg-green-700 w-32 rounded-xl" />
         <div className=" mx-2 bg-white w-44 rounded-xl" />
-        <div className=" mx-2 bg-green-600 w-52 rounded-xl" />
+        <div className=" mx-2 bg-green-700 w-52 rounded-xl" />
         <div className=" mx-2 bg-white w-32 rounded-xl" />
-        <div className=" mx-2 bg-green-600 w-20 rounded-xl" />
+        <div className=" mx-2 bg-green-700 w-20 rounded-xl" />
         <div className=" mx-2 bg-white w-44 rounded-xl" />
-        <div className=" mx-2 bg-green-600 w-16 rounded-xl" />
+        <div className=" mx-2 bg-green-700 w-16 rounded-xl" />
+        <div className=" mx-2 bg-white w-20 rounded-xl" />
+        <div className=" mx-2 bg-green-700 w-44 rounded-xl" />
+        <div className=" mx-2 bg-white w-32 rounded-xl" />
+        <div className=" mx-2 bg-green-700 w-16 rounded-xl" />
     </>
   );
 }
