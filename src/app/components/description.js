2e7102d770fabda39065fce00b49f9594f79c838
izaack107 <izaack107@gmail.com>
2024-04-15 11:25:02 -0600

    Update Description component

--- src/app/components/description.js
+++ src/app/components/description.js
@@ -3,8 +3,7 @@
 export default function Description() {
     return (
       <section id="description" className="relative isolate overflow-hidden bg-black px-6 py-24 sm:py-32 lg:px-8">
-        <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),black)] opacity-20" />
-        <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-black shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
+        <div className="absolute inset-0 -z-10 bg-black opacity-20" />
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
           <img className="mx-auto h-14" src="https://imgur.com/e2TOmcF.jpg" alt="" />
           <figure className="mt-10">
