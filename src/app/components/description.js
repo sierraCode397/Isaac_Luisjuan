8f93d386d7ffeaa390567cb70d0762927daf2741
izaack107 <izaack107@gmail.com>
2024-04-21 15:33:32 -0600

    Update main component and added animation

--- src/app/components/description.js
+++ src/app/components/description.js
@@ -2,7 +2,11 @@ import Cobe from "./../common/cobe";
 
 export default function Description() {
     return (
-      <section id="description" className="pt-16 bg-black profile-page">
+      <section 
+      id="description" 
+      className="mb-16 pt-16 bg-black profile-page"
+      style={{height: "500px"}}
+      >
         <div className="flex flex-wrap items-center justify-evenly px-6">
           <section
             className="relative block"
