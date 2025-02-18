894682da2f0c6491073b40bed5368063bdb01521
izaack107 <izaack107@gmail.com>
2025-02-18 16:27:38 -0600

    Upload my certificates

--- src/app/components/description.js
+++ src/app/components/description.js
@@ -8,24 +8,30 @@ export default function Description() {
       className={styles.description}
     >
       <div className="flex flex-wrap items-center justify-evenly px-6">
-        <p
-          className="text-white hover:animate-bounce hidden lg:block border border-solid border-white rounded p-4 py-12"
-          style={{ width: "170px" }}
-        >
-          Become the architect of your own world with code.
-        </p>
+      <div className="">
+              <a href="https://www.credly.com/badges/163e9d9b-1b19-48f1-86af-ed476793fbe6" className="hover:animate-bounce block relative h-26 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-fill object-center hidden lg:block mx-auto w-48 h-34 md:w-48 md:h-34 lg:w-52 lg:h-34 block"
+                  src="https://imgur.com/KS52t9q.png"
+                />
+              </a>
+        </div>
         <section
           className=" "
           style={{ height: "500px", width: "500px" }}
         >
           <Cobe />
         </section>
-        <p
-          className="text-white hover:animate-bounce hidden lg:block border border-solid border-white rounded p-4 py-12"
-          style={{ width: "170px" }}
-        >
-          The limits of my lenguage means the limits of my world.
-        </p>
+        <div className="">
+              <a href="https://www.credly.com/badges/e6d53ea3-5211-4d49-8d57-de6ee56797ec" className="hover:animate-bounce block relative h-26 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-fill object-center mx-auto mb-24 md:mb-0 w-48 h-34 md:w-48 md:h-34 lg:w-52 lg:h-34 block"
+                  src="https://imgur.com/f9i2YwA.png"
+                />
+              </a>
+        </div>
       </div>
     </section>
   );
