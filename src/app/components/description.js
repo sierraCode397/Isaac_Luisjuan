1025b2aca48bfa186013b81a40ef0ccd8c78e128
Isaac Luisjuan <105889543+sierraCode397@users.noreply.github.com>
2025-04-18 10:47:55 -0600

    AWS Academy Cloud Architecting

--- src/app/components/description.js
+++ src/app/components/description.js
@@ -9,11 +9,11 @@ export default function Description() {
     >
       <div className="flex flex-wrap items-center justify-evenly px-6">
       <div className="">
-              <a href="https://www.credly.com/badges/163e9d9b-1b19-48f1-86af-ed476793fbe6" className="hover:animate-bounce block relative h-26 rounded overflow-hidden">
+              <a href="https://www.credly.com/badges/88497f98-cacb-4c8a-9a37-750c61709aa0/public_url" className="hover:animate-bounce block relative h-26 rounded overflow-hidden">
                 <img
                   alt="ecommerce"
                   className="object-fill object-center hidden lg:block mx-auto w-48 h-34 md:w-48 md:h-34 lg:w-52 lg:h-34 block"
-                  src="https://imgur.com/KS52t9q.png"
+                  src="https://imgur.com/MUFADKK.png"
                 />
               </a>
         </div>
